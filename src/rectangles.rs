170ac4b737fc96b8a3fae0979b031ed9f25f7a02
konafx <afxlieonon@gmail.com>
2020-06-04 14:05:51 +0900

    struct & impl

--- src/rectangles.rs
+++ src/rectangles.rs
@@ -1,13 +1,21 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
-    let width1 = 30;
-    let height1 = 50;
+    let rect1 = Rectangle { width: 30, height: 50};
 
     println!(
         "The area of the rectangle is {} square pixels",
-        area(width1, height1)
+        rect1.area()
         );
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
-}
